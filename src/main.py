1ee50215cece0608069b43cf88dd60595b2ff394
Archy de Berker <archy@carbonchain.io>
2022-09-30 21:06:33 +0100

    Integrate gmaps autocomplete

--- src/main.py
+++ src/main.py
@@ -7,4 +7,5 @@ import roof
 # has_gas, heating_system = usage.render()
 # solar_orientation, roof_area_m2 = solar.render()
 
-roof.roof_mapper(800, 800)
+roof.roof_mapper(800, 400)
+st.text_input('demo')
--- src/roof.py
+++ src/roof.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
+import folium
 import leafmap.foliumap as leafmap
 import streamlit as st
 from folium.plugins import Draw
 from streamlit_folium import st_folium
 import numpy as np
-
-m = leafmap.Map(google_map="SATELLITE")
+from place_search import place_search
 
 
 def shoelace(x_y):
@@ -35,12 +35,25 @@ class Polygon:
         return shoelace(self.points)
 
 
-def roof_mapper(width: int, height: int) -> List[Polygon]:
+DEFAULT_LOCATION = dict(lat=0, lng=0, address="")
+
+
+def roof_mapper(width: int, height: int) -> Optional[List[Polygon]]:
     """
     Render a map, returning co-ordinates of any shapes drawn upon the map
     """
 
+    selected_location = place_search(my_input_value="yeehaa")
+
+    centre = [selected_location["lat"], selected_location["lng"]] if selected_location else [50, 0]
+    m = leafmap.Map(google_map="SATELLITE", location=centre,
+                    zoom_start=22 if selected_location else 2)
+    if selected_location:
+        marker = folium.Marker(
+            [selected_location["lat"], selected_location["lng"]], popup=selected_location["address"]
+        ).add_to(m)
+
     map = st_folium(m, width=width, height=height)
     st.write(map)
-
-    return [Polygon(points=drawing['geometry']['coordinates']) for drawing in map['all_drawings']]
+    if map["all_drawings"]:
+        return [Polygon(points=drawing["geometry"]["coordinates"]) for drawing in map["all_drawings"]]
