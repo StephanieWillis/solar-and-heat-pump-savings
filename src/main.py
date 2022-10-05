b8abbd9b94fb98422944527f62ddd9869858e35f
steph_willis <steph.willis@sero.life>
2022-10-05 08:31:25 +0100

    Reorder tabs so all questions at the start

--- src/main.py
+++ src/main.py
@@ -8,18 +8,16 @@ st.write("This tool is to help you get a rough idea of how much money and carbon
          " tool merely aims to help you sense for what the impacts might be. Throughout the tool we have made estimates"
          " of various values, but you can overwrite them if you have better info. ")
 
-use_qs_tab, use_outputs_tab, solar_qs_tab, solar_potential_tab = st.tabs(["Usage Questions", "Baseline Usage Outputs",
-                                                                          "Solar Questions", "Solar Outputs"])
+use_qs_tab, solar_qs_tab, use_outputs_tab, potential_tab = st.tabs(["Usage Questions",
+                                                                    "Solar Questions",
+                                                                    "Your Current Energy Use",
+                                                                    "Your Electrified Home"])
 with use_qs_tab:
     house, heating_system = usage.render_questions()
+    with solar_qs_tab:
+        solar_install = solar.render_questions()
 with use_outputs_tab:
     usage.render_outputs(house=house, heating_system=heating_system)
-with solar_qs_tab:
-    solar_install = solar.render_questions()
-with solar_potential_tab:
+with potential_tab:
     solar.render_outputs(solar_install=solar_install)
-
-# Print baseline (and let fiddle)
-# Run case with HP if no HP
-# Run case with solar
-# Run case with HP + solar
+#     TODO: change this to be Electrified home
