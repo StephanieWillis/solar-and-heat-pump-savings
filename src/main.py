901f04f4959f6bd5a8c05d4032c98192d4b96470
steph_willis <steph.willis@sero.life>
2022-10-08 10:08:19 +0100

    Put info text in info box

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import streamlit as st
 import usage
 import solar
 
-st.write("This tool is to help you get a rough idea of how much money and carbon you might save by installing a "
+st.info("This tool is to help you get a rough idea of how much money and carbon you might save by installing a "
          "heat pump and/or solar panels. The real costs and performance will depend on the specifics of your home."
          " Throughout the tool we have made estimates"
          " of various values, but you can overwrite them if you have better info. ")
