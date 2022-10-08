70854de42706a025f660a98ca76fd6a6dc5f3828
steph_willis <steph.willis@sero.life>
2022-10-08 11:42:21 +0100

    Make clear where user can overwrite assumptions

--- src/main.py
+++ src/main.py
@@ -4,9 +4,9 @@ import usage
 import solar
 
 st.info("This tool is to help you get a rough idea of how much money and carbon you might save by installing a "
-         "heat pump and/or solar panels. The real costs and performance will depend on the specifics of your home."
-         " Throughout the tool we have made estimates"
-         " of various values, but you can overwrite them if you have better info. ")
+        "heat pump and/or solar panels. The real costs and performance will depend on the specifics of your home."
+        " Throughout the tool we have made estimates of various values, but you can overwrite them at the bottom "
+        "of the Savings Potential page if you have better info. ")
 
 use_qs_tab, solar_qs_tab, savings_tab = st.tabs(["Usage Questions", "Solar Questions", "Your Savings Potential"])
 with use_qs_tab:
@@ -16,4 +16,3 @@ with solar_qs_tab:
 with savings_tab:
     house = usage.render_outputs(house=house)
     # solar.render_outputs(solar_install=solar_install)
-
