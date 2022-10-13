fbb2a89478e230fe697804030085c2799a409aaa
Archy de Berker <archy@carbonchain.io>
2022-10-13 21:35:35 +0100

    Wire up roof selection to solar module

--- src/constants.py
+++ src/constants.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 import pandas as pd
 
-HOUSE_TYPES = ['Terrace', 'Semi-detached', 'Detached', 'Flat']
+HOUSE_TYPES = ["Terrace", "Semi-detached", "Detached", "Flat"]
 
 BASE_YEAR_HALF_HOUR_INDEX = pd.date_range(start="2020-01-01", end="2021-01-01", freq="30T")
 EMPTY_TIMESERIES = pd.Series(index=BASE_YEAR_HALF_HOUR_INDEX, data=0)
@@ -11,7 +11,7 @@ EMPTY_TIMESERIES = pd.Series(index=BASE_YEAR_HALF_HOUR_INDEX, data=0)
 @dataclass
 class Fuel:
     name: str
-    units: str = 'kwh'
+    units: str = "kwh"
     converter_consumption_units_to_kwh: float = 1
 
     def convert_kwh_to_fuel_units(self, value_kwh: [float | pd.Series | pd.DataFrame]):
@@ -25,9 +25,9 @@ class Fuel:
 
 kwh_PER_LITRE_OF_OIL = 10.35
 # https://www.thegreenage.co.uk/is-heating-oil-a-cheap-way-to-heat-my-home/
-ELECTRICITY = Fuel('electricity')
-GAS = Fuel(name='gas')
-OIL = Fuel(name='oil', units='litres', converter_consumption_units_to_kwh=kwh_PER_LITRE_OF_OIL)
+ELECTRICITY = Fuel("electricity")
+GAS = Fuel(name="gas")
+OIL = Fuel(name="oil", units="litres", converter_consumption_units_to_kwh=kwh_PER_LITRE_OF_OIL)
 FUELS = [ELECTRICITY, GAS, OIL]
 
 
@@ -39,18 +39,11 @@ class HeatingConstants:
 
 
 DEFAULT_HEATING_CONSTANTS = {
-    'Gas boiler': HeatingConstants(space_heating_efficiency=0.85,
-                                   water_heating_efficiency=0.8,
-                                   fuel=GAS),
-    'Oil boiler': HeatingConstants(space_heating_efficiency=0.8,
-                                   water_heating_efficiency=0.75,
-                                   fuel=OIL),
-    'Direct electric': HeatingConstants(space_heating_efficiency=1.0,
-                                        water_heating_efficiency=1.0,
-                                        fuel=ELECTRICITY),
-    'Heat pump': HeatingConstants(space_heating_efficiency=3.5,
-                                  water_heating_efficiency=3.0,
-                                  fuel=ELECTRICITY)}
+    "Gas boiler": HeatingConstants(space_heating_efficiency=0.85, water_heating_efficiency=0.8, fuel=GAS),
+    "Oil boiler": HeatingConstants(space_heating_efficiency=0.8, water_heating_efficiency=0.75, fuel=OIL),
+    "Direct electric": HeatingConstants(space_heating_efficiency=1.0, water_heating_efficiency=1.0, fuel=ELECTRICITY),
+    "Heat pump": HeatingConstants(space_heating_efficiency=3.5, water_heating_efficiency=3.0, fuel=ELECTRICITY),
+}
 
 
 @dataclass
@@ -62,22 +55,20 @@ class TariffConstants:
     p_per_L_oil: float
 
 
-STANDARD_TARIFF = TariffConstants(p_per_kwh_gas=10.3,
-                                  p_per_kwh_elec=34.0,
-                                  p_per_day_gas=28.0,
-                                  p_per_day_elec=46.0,
-                                  p_per_L_oil=95.0)
+STANDARD_TARIFF = TariffConstants(
+    p_per_kwh_gas=10.3, p_per_kwh_elec=34.0, p_per_day_gas=28.0, p_per_day_elec=46.0, p_per_L_oil=95.0
+)
 
 
 class SolarConstants:
-    SOLAR_ORIENTATIONS = ['South', 'Southwest', 'West', 'Northwest', 'North', 'Northeast', 'East', 'Southeast']
+    SOLAR_ORIENTATIONS = ["South", "Southwest", "West", "Northwest", "North", "Northeast", "East", "Southeast"]
     MIN_ROOF_AREA = 0
     MAX_ROOF_AREA = 20
     DEFAULT_ROOF_AREA = 20
     PANEL_HEIGHT_M = 1.9
     PANEL_WIDTH_M = 1.0
+    PANEL_AREA = PANEL_HEIGHT_M * PANEL_WIDTH_M
     KW_PEAK_PER_PANEL = 0.35  # output with incident radiation of 1kW/m2
     # Panel dimensions and kW_peak from https://www.greenmatch.co.uk/blog/how-many-solar-panels-do-i-need
     PCT_OF_DIMENSION_USABLE = 0.9  # a guess
-
-
+    PERCENT_ROOF_USABLE = 0.8  # complete guess
--- src/solar.py
+++ src/solar.py
@@ -1,63 +1,62 @@
+from math import floor
 from typing import Tuple
 
 import pandas as pd
 import numpy as np
 import streamlit as st
 
+from constants import SolarConstants
 import constants
 import usage
 import roof
 
 
-def render_questions() -> 'Solar':
+def render_questions() -> "Solar":
     st.header("Your solar potential")
 
-    st.write("Search for your home below and draw a square where you think solar panels might go on your most South"
-             "facing roof. Use the hexagonal tool to draw the square.  ")
+    st.write(
+        "Search for your home below and draw a square where you think solar panels might go on your most South"
+        "facing roof. Use the hexagonal tool to draw the square.  "
+    )
     polygons = roof.roof_mapper(800, 400)
-    # returns list of list of polygons
-    if polygons:
-        st.write([p.dimensions for p in polygons])
 
-    # TODO: get width and height from polygons once figured out how to get working
     postcode: str = st.text_input("Postcode:")
-    orientation: str = st.selectbox('Solar Orientation', constants.SolarConstants.SOLAR_ORIENTATIONS)
-    roof_height = st.number_input(label='Roof height (m)', min_value=0.0, max_value=10.0, value=2.0)
-    roof_width = st.number_input(label='Roof width (m)', min_value=0.0, max_value=20.0, value=8.0)
+    orientation: str = st.selectbox("Solar Orientation", SolarConstants.SOLAR_ORIENTATIONS)
 
-    solar_install = Solar(orientation=orientation, roof_width_m=roof_width, roof_height_m=roof_height,
-                          postcode=postcode)
+    solar_install = Solar(orientation=orientation, roof_area=polygons.area if polygons else 0, postcode=postcode)
+    if polygons:
+        st.write(f"We estimate you can fit {solar_install.number_of_panels} on your roof!")
+
     return solar_install
 
 
-def render_outputs(solar_install: 'Solar'):
+def render_outputs(solar_install: "Solar"):
     st.header("Solar potential")
-    st.write(f'Your roof faces {solar_install.orientation} and could fit  {solar_install.number_of_panels} panels')
-    st.write(f'That amounts to {round(solar_install.peak_capacity_kW_out_per_kW_in_per_m2, 1)} kW of peak capacity.')
-    st.write(f'We estimate that would generate {int(solar_install.generation.annual_sum_kwh):,}  kwh of electricity per year')
+    st.write(f"Your roof faces {solar_install.orientation} and could fit  {solar_install.number_of_panels} panels")
+    st.write(f"That amounts to {round(solar_install.peak_capacity_kW_out_per_kW_in_per_m2, 1)} kW of peak capacity.")
+    st.write(
+        f"We estimate that would generate {int(solar_install.generation.annual_sum_kwh):,}  kwh of electricity per year"
+    )
 
 
 class Solar:
-
-    def __init__(self, orientation: str, roof_height_m: float, roof_width_m: float, postcode: str):
+    def __init__(self, orientation: str, roof_area: float, postcode: str):
 
         self.orientation = orientation
-        self.number_of_panels = self.get_number_of_panels(roof_width_m, roof_height_m)
+        self.number_of_panels = self.get_number_of_panels(roof_area)
         self.peak_capacity_kW_out_per_kW_in_per_m2 = self.get_peak_capacity(
-            kWp_per_panel=constants.SolarConstants.KW_PEAK_PER_PANEL)
+            kWp_per_panel=SolarConstants.KW_PEAK_PER_PANEL
+        )
         profile_kwh_per_m2 = self.get_generation_profile(postcode)
-        self.generation = usage.Consumption(profile_kwh=profile_kwh_per_m2 * self.peak_capacity_kW_out_per_kW_in_per_m2,
-                                            fuel=constants.ELECTRICITY)
+        self.generation = usage.Consumption(
+            profile_kwh=profile_kwh_per_m2 * self.peak_capacity_kW_out_per_kW_in_per_m2, fuel=constants.ELECTRICITY
+        )
 
     @staticmethod
-    def get_number_of_panels(roof_width_m: float, roof_height_m: float) -> float:
-        height_available_for_panels = roof_height_m * constants.SolarConstants.PCT_OF_DIMENSION_USABLE
-        width_available_for_panels = roof_width_m * constants.SolarConstants.PCT_OF_DIMENSION_USABLE
+    def get_number_of_panels(roof_area) -> int:
+        usable_area = roof_area * SolarConstants.PERCENT_ROOF_USABLE
+        number_of_panels = floor(usable_area / SolarConstants.PANEL_AREA)
 
-        number_of_rows = int(height_available_for_panels/constants.SolarConstants.PANEL_HEIGHT_M)
-        number_of_columns = int(width_available_for_panels/constants.SolarConstants.PANEL_WIDTH_M)
-
-        number_of_panels = number_of_columns * number_of_rows
         return number_of_panels
 
     def get_peak_capacity(self, kWp_per_panel: float):
@@ -71,16 +70,10 @@ class Solar:
         idx = constants.BASE_YEAR_HALF_HOUR_INDEX
         minute_of_the_day = idx.hour * 60 + idx.minute
         kW_per_m2_peak = 0.3
-        generation = - np.cos(minute_of_the_day * np.pi * 2/(24 * 60)) * kW_per_m2_peak
+        generation = -np.cos(minute_of_the_day * np.pi * 2 / (24 * 60)) * kW_per_m2_peak
         profile_kW_per_m2 = pd.Series(index=constants.BASE_YEAR_HALF_HOUR_INDEX, data=generation)
         profile_kW_per_m2.loc[profile_kW_per_m2 < 0] = 0
-        profile_kwh_per_m2 = profile_kW_per_m2/2  # because we know the time base is half hourly
+        profile_kwh_per_m2 = profile_kW_per_m2 / 2  # because we know the time base is half hourly
         # If not importing this from elsewhere think about whether need to integrate (take the previous half hour)
 
         return profile_kwh_per_m2
-
-
-
-
-
-
