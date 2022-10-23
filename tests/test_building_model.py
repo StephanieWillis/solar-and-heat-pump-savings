ca99b85a8e5889052d36798d67cf9a90679fed93
steph_willis <steph.willis@sero.life>
2022-10-23 13:14:59 +0100

    Move solar tests into a seperate file and split them out

--- tests/test_building_model.py
+++ tests/test_building_model.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import numpy as np
 
 from src import constants
@@ -6,10 +5,12 @@ from src import building_model
 from src import solar
 
 
-def test_set_up_house():
+def test_set_up_from_heating_name():
     house_floor_area_m2 = 100
     house_type = "terrace"
     envelope = building_model.BuildingEnvelope(house_type=house_type, floor_area_m2=house_floor_area_m2)
+
+    # heat pump
     hp_house = building_model.House.set_up_from_heating_name(envelope=envelope, heating_name='Heat pump')
     hp_house_elec_consumption = hp_house.consumption_per_fuel['electricity']
 
@@ -24,12 +25,14 @@ def test_set_up_house():
     assert hp_house.total_annual_consumption_kwh == hp_house.energy_and_bills_df['Your annual energy use kwh'].iloc[0]
     assert hp_house.total_annual_bill == hp_house.energy_and_bills_df['Your annual energy bill £'].iloc[0]
 
+    # gas boiler
     gas_house = building_model.House.set_up_from_heating_name(envelope=envelope, heating_name='Gas boiler')
     assert gas_house.has_multiple_fuels
 
     assert gas_house.envelope == hp_house.envelope
     assert list(gas_house.consumption_per_fuel.keys()) == ['electricity', 'gas']
 
+    # oil boiler
     oil_house = building_model.House.set_up_from_heating_name(envelope=envelope, heating_name='Oil boiler')
     assert (oil_house.consumption_per_fuel['oil'].overall.annual_sum_fuel_units <
             oil_house.consumption_per_fuel['oil'].overall.annual_sum_kwh)
@@ -40,30 +43,6 @@ def test_set_up_house():
     assert oil_house.solar.generation.overall.annual_sum_kwh == 0
 
     return hp_house, gas_house, oil_house
-
-
-def test_solar():
-    orientation = 'South'
-    roof_area = 14
-    solar_install = solar.Solar(orientation=orientation,
-                                roof_plan_area=roof_area)
-    assert solar_install.generation.fuel.name == 'electricity'
-    assert solar_install.number_of_panels > 0
-    assert (solar_install.generation.exported.annual_sum_kwh == solar_install.generation.overall.annual_sum_kwh)
-    assert (solar_install.generation.imported.annual_sum_kwh == 0)
-    assert abs(solar_install.generation.overall.annual_sum_kwh) > 0
-
-    assert solar_install.orientation == orientation
-    assert solar_install.generation.fuel.units == "kwh"
-    assert solar_install.generation.fuel.name == 'electricity'
-    assert isinstance(solar_install.number_of_panels, int)
-    assert isinstance(solar_install.peak_capacity_kw_out_per_kw_in_per_m2, float)
-    assert isinstance(solar_install.generation.overall.profile_kwh, pd.Series)
-    assert isinstance(solar_install.generation.overall.annual_sum_kwh, float)
-
-    # fig = px.line(profile.loc["2020-01-01": "2020-01-03"])
-    # fig.show()
-    return solar_install
 
 
 def test_upgrade_buildings():
@@ -86,3 +65,4 @@ def test_upgrade_buildings():
                                    + both_house.consumption_per_fuel['electricity'].imported.annual_sum_kwh,
                                    both_house.consumption_per_fuel['electricity'].overall.annual_sum_kwh)
     assert hp_house.total_annual_bill > both_house.total_annual_bill
+
---
+++ tests/test_solar.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import plotly.express as px
+
+import solar
+
+
+def test_get_number_of_panels():
+    orientation = 'South'
+    roof_area = 14
+    solar_install = solar.Solar(orientation=orientation,
+                                roof_plan_area=roof_area)
+    assert solar_install.number_of_panels > 0
+    assert isinstance(solar_install.number_of_panels, int)
+
+
+def test_generation():
+    orientation = 'South'
+    roof_area = 14
+    solar_install = solar.Solar(orientation=orientation,
+                                roof_plan_area=roof_area)
+    assert solar_install.generation.fuel.name == 'electricity'
+    assert (solar_install.generation.exported.annual_sum_kwh == solar_install.generation.overall.annual_sum_kwh)
+    assert (solar_install.generation.imported.annual_sum_kwh == 0)
+    assert abs(solar_install.generation.overall.annual_sum_kwh) > 0
+
+    assert solar_install.generation.fuel.units == "kwh"
+    assert solar_install.generation.fuel.name == 'electricity'
+
+    assert isinstance(solar_install.generation.overall.profile_kwh, pd.Series)
+    assert isinstance(solar_install.generation.overall.annual_sum_kwh, float)
+
+    fig = px.line(solar_install.generation.overall.profile_kwh.loc["2020-01-01": "2020-01-03"])
+    fig.show()
+
+
+def test_peak_capacity_kw_out_per_kw_in_per_m2():
+    orientation = 'North'
+    roof_area = 10.5
+    solar_install = solar.Solar(orientation=orientation,
+                                roof_plan_area=roof_area)
+    assert isinstance(solar_install.peak_capacity_kw_out_per_kw_in_per_m2, float)
