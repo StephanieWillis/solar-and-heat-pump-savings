8f6f3482b4f1f73491943322b35286413246b241
steph_willis <steph.willis@sero.life>
2022-11-06 19:39:13 +0000

    Try and resolve place search path issue

--- src/place_search/__init__.py
+++ src/place_search/__init__.py
@@ -1,5 +1,9 @@
 import streamlit.components.v1 as components
+from pathlib import Path
+
+THIS_FILE = Path(__file__)
+
 place_search = components.declare_component(
     "place_search",
-    path="./place_search"
+    path=Path(THIS_FILE.parent /"place_search")
 )
